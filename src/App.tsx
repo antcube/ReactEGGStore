0f3cae2e0ee2eac4a34125c48b13033f4ed3fba5
antcube <aqr885@gmail.com>
2024-05-28 20:53:56 -0500

    iniciando con React

---
+++ src/App.tsx
@@ -0,0 +1,218 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      < NavBar/>
+      < Hero/>
+
+      <main>
+        <div className="product-container" id="products">
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+          <a className="product-card" href="./details.html">
+            <img
+              className="product-img"
+              src="https://i.postimg.cc/kX8PKZpq/ipad2.jpg"
+              alt="iPad Pro 12.9"
+            />
+            <div className="product-info">
+              <span className="product-title">iPad Pro 12.9</span>
+              <span className="product-description">Silver</span>
+              <div className="product-price-block">
+                <span className="product-price">900000</span>
+                <span className="product-discount">50% Off</span>
+              </div>
+              <div className="product-tax-policy">
+                Incluye impuesto País y percepción AFIP
+              </div>
+            </div>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <div id="footer" className="footer-container">
+          <ul className="footer-ul">
+            <li className="footer-main-item">
+              <a className="footer-a" href="./index.html">
+                Ofertas
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Laptops
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Audio
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Auriculares
+              </a>
+            </li>
+          </ul>
+          <ul className="footer-ul">
+            <li className="footer-main-item">
+              <a className="footer-a" href="./index.html">
+                Cómo comprar
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Formas de pago
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Envios
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Devoluciones
+              </a>
+            </li>
+          </ul>
+          <ul className="footer-ul">
+            <li className="footer-main-item">
+              <a className="footer-a" href="./index.html">
+                Costos y tarifas
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Impuestos
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Facturación
+              </a>
+            </li>
+          </ul>
+          <ul className="footer-ul">
+            <li className="footer-main-item">
+              <a className="footer-a" href="./index.html">
+                Mis pedidos
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Pedir nuevamente
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="./index.html">
+                Lista de deseos
+              </a>
+            </li>
+          </ul>
+          <ul className="footer-ul">
+            <li className="footer-main-item">
+              <a className="footer-a" href="./index.html">
+                Garantía
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-copyright">Curso de HTML + CSS 2023</div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
